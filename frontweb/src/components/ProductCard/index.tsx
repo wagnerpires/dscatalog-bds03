65744afae2b6569a02e88bf28646659229268e7c
Wagner Pires <wagnerpires@hotmail.com>
2021-07-18 18:52:39 -0300

    Props ProductCard

--- frontweb/src/components/ProductCard/index.tsx
+++ frontweb/src/components/ProductCard/index.tsx
@@ -1,21 +1,23 @@
+import ProductPrice from 'components/ProductPrice';
+import { Product } from 'types/product';
 import './styles.css';
 
-import ProductImg from 'assets/images/product.png';
-import ProductPrice from 'components/ProductPrice';
+type Props = {
+  product: Product;
+};
 
-const ProductCard = () => {
-
-    return (
-        <div className="base-card product-card">
-            <div className="card-top-container">
-                <img src={ProductImg} alt="Nome do produto" />
-            </div>
-            <div className="card-bottom-container">
-                <h6>Nome do produto</h6>
-                <ProductPrice />
-            </div>
-        </div>
-    );
-}
+const ProductCard = ({ product }: Props) => {
+  return (
+    <div className="base-card product-card">
+      <div className="card-top-container">
+        <img src={product.imgUrl} alt={product.name} />
+      </div>
+      <div className="card-bottom-container">
+        <h6>{product.name}</h6>
+        <ProductPrice price={product.price} />
+      </div>
+    </div>
+  );
+};
 
 export default ProductCard;
